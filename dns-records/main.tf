13c495c2f2812dc2eb09d4e981811b2a8ce0b66b
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-08-26 21:38:48 +0530

    dns records

---
+++ dns-records/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_route53_record" "dns_records" {
+  for_each = var.components
+  zone_id  = var.zone_id
+  name     = "${each.key}-dev"
+  type     = "A"
+  ttl      = 30
+  records  = [each.value]
+}
+
+variable "zone_id" {
+  default = "Z03809721NR4CZZKYMR0T"
+}
+
+variable "components" {
+  default = {
+    frontend    = "10.0.1.8"
+    mongodb     = "10.0.1.9"
+    catalogue   = "10.0.1.13"
+    user        = "10.0.1.7"
+    redis       = "10.0.1.4"
+    cart        = "10.0.1.11"
+    mysql       = "10.0.1.12"
+    shipping    = "10.0.1.5"
+    rabbitmq    = "10.0.1.10"
+    payment     = "10.0.1.6"
+  }
+}
