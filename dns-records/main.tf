476ee4ca83e148010e2a4860b657d12703bda0e2
Sandeep Kumar Penta <tsbsandeepkumar@gmail.com>
2026-08-28 18:43:13 +0530

    ansible secrets

--- dns-records/main.tf
+++ dns-records/main.tf
@@ -19,14 +19,14 @@ variable "zone_id" {
 variable "components" {
   default = {
     frontend    = "10.0.1.8"
-    mongodb     = "10.0.1.9"
-    catalogue   = "10.0.1.13"
-    user        = "10.0.1.7"
-    redis       = "10.0.1.4"
-    cart        = "10.0.1.11"
-    mysql       = "10.0.1.12"
-    shipping    = "10.0.1.5"
+    mongodb     = "10.0.1.4"
+    catalogue   = "10.0.1.6"
+    user        = "10.0.1.13"
+    redis       = "10.0.1.12"
+    cart        = "10.0.1.13"
+    mysql       = "10.0.1.11"
+    shipping    = "10.0.1.7"
     rabbitmq    = "10.0.1.10"
-    payment     = "10.0.1.6"
+    payment     = "10.0.1.5"
   }
 }
